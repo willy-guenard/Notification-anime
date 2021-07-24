5bb57ca09bc62ce3c6349f620d57a937c7640b8a
willy.g <willy.guenard@outlook.fr>
2021-07-24 02:56:04 +0200

    4

--- a faire/pour recup dans adkami.js
+++ a faire/pour recup dans adkami.js
@@ -1,28 +1,54 @@
 "col-12 episode"
 
-let boite = document.getElementsByClassName('col-12 episode');
+  let boite = document.getElementsByClassName('col-12 episode');
 
-boite = Array.prototype.slice.call(boite, 0);
+  boite = Array.prototype.slice.call(boite, 0);
 
-// recupe name des episode
-boite.map(function (elem) { return elem.textContent; });
+  // recupe name des episode
+  boite.map(function (elem) { return elem.textContent; });
 
-//separer 1 anime
-let boite2 = boite[1];
+  //separer 1 anime
+  let boite2 = boite[1];
 
-let boite3 = boite2.firstElementChild.currentSrc;
+  let boite3 = boite2.firstElementChild.currentSrc;
 
 
-let elem = 15;
-boite.map(function (elem)
-    {
-      let boite1 = elem.textContent;
-      let boite2 = boite1[elem];
-      boite2.firstElementChild.currentSrc;
-    });
+  let elem = 15;
+  boite.map(function (elem)
+      {
+        let boite1 = elem.textContent;
+        let boite2 = boite1[elem];
+        boite2.firstElementChild.currentSrc;
+      });
 
 
 chercher a recuperais l'img par rapport au name
+
+Titre 			class title
+episode			class epis
+type_episode		class epis
+Voice			class epis
+picture_url 		<img
+heure			class date_hour
+day			je les deja
+
+
+let boite = document.getElementsByClassName('colone');
+let lundi = boite[0]
+let detectiveconan = lundi.children[1]
+
+
+innerText
+
+detectiveconan.childNodes
+
+que le premier de lundi a script
+
+detectiveconan.childNodes[1].children[1].currentSrc
+detectiveconan.childNodes[1].children[2].innerText
+detectiveconan.childNodes[1].children[3].children[0].innerText
+detectiveconan.childNodes[1].children[3].children[1].innerText
+
 
 
 
