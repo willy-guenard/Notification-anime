254edfa73c0391fd50e3f91364699e51fd5acea2
willy.g <willy.guenard@outlook.fr>
2021-09-22 21:23:13 +0200

    fe

--- WindowsSecondaire/secondeWindow/seconpreload.js
+++
@@ -1,8 +0,0 @@
-const {ipcRenderer} = require("electron");
-
-window.addEventListener('DOMContentLoaded', () => {
-
-  const tUrl = document.querySelector("#Turl");
-
-  tUrl.addEventListener('click', function(){ipcRenderer.send('Cannel-Url', "url" )});
-})
